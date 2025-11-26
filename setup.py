5e7bb1234772271da6e836909aa1d96e0a53ad18
Alex Brynes <alexander.brynes@stfc.ac.uk>
2025-11-26 13:12:20 +0000

    update setup configs

--- setup.py
+++ setup.py
@@ -1,37 +1,54 @@
 from setuptools import setup, find_packages
-import versioneer
 
-with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
-
-requirements = [
-    "deepdiff>=5",
-    "h5py>=2.10",
-    "munch>=2.5",
-    "numpy>=1.19,<2",
-    "tqdm>=4",
-    "PyQt5>=5.1",
-    "PyYAML>=5.3",
-    "mpl-axes-aligner>=1.1",
-    "lox>=0.11",
-    "fastKDE<2",
-    "pydantic>=2.5.3",
-    "attrs>=23.2.0",
-    "easygdf>=2.1.1",
-]
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
 
 setup(
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    name="simba-accelerator",  # must match pyproject.toml
     packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "deepdiff>=5",
+        "h5py>=2.10",
+        "munch>=2.5",
+        "numpy>=2",
+        "tqdm>=4",
+        "PyQt5>=5.1",
+        "PyYAML>=5.3",
+        "mpl-axes-aligner>=1.1",
+        "lox>=0.11",
+        "fastKDE>=2.1.5",
+        "pydantic>=2.5.3",
+        "attrs>=23.2.0",
+        "ocelot-desy==25.06.0",
+        "scipy>=1.5",
+        "soliday.sdds",
+        "easygdf>=2.1.1",
+        "xsuite>=0.39.0",
+        "paramiko",
+        "deap",
+        "pyqtgraph",
+        "numba",
+        "pyfftw",
+        "numexpr",
+        "cheetah-accelerator>=0.7.5",
+        "openpmd-beamphysics>=0.10",
+        "xopt",
+    ]
     package_data={
-        "SimulationFramework": [
+        "simba": [
             "Codes/*.yaml",
             "Codes/Elegant/*.yaml",
+            "Codes/Cheetah/*.yaml",
+            "Codes/Generators/*.yaml",
             "Codes/CSRTrack/*.yaml",
             "Codes/Ocelot/*.yaml",
-            "Elements/*.yaml",
+            "Codes/Genesis/*.yaml",
+            "Codes/OPAL/*.yaml",
             "*.yaml",
         ]
     },
+        python_requires=">=3.10",
+    long_description=readme,
+    long_description_content_type="text/markdown",
 )
