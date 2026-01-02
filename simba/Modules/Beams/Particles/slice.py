fixed to improve genesis beam input and twiss calculation

simba/Modules/Beams/Particles/slice.py
@@ -522,7 +522,7 @@ class slice(BaseModel):
         np.ndarray
             Slice longitudinal bins
         """
-        return np.array(self.slice_bins)
+        return np.array(list(reversed(np.array(self.slice_bins))))
 
     @property
     def slice_horizontal_emittance(self) -> UnitValue:

simba/Modules/Beams/genesis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import h5py
 from pmd_beamphysics import ParticleGroup
 from pmd_beamphysics.interfaces.genesis import (
     genesis4_par_to_data,
@@ -7,6 +8,7 @@ from pmd_beamphysics.interfaces.genesis import (
 )
 from .openpmd import read_openpmd_beam_file, write_openpmd_beam_file
 from ..units import UnitValue
+from .. import pmd_units
 
 def read_genesis_beam_file(
         self,
@@ -58,8 +60,19 @@ def write_genesis_beam_distribution(self, filename: str):
     pg = write_openpmd_beam_file(self, filename)
     write_genesis4_distribution(pg, filename.replace('openpmd', 'genesis'))
 
-def write_genesis_beam_file(self, filename: str, n_slice: int | None = None):
+def write_genesis_beam_file(self, filename: str, n_slice: int = 10):
     pg = write_openpmd_beam_file(self, filename)
     pg.t = np.zeros(len(pg.z))
     pg.z -= np.mean(pg.z)
+    with h5py.File(filename.replace("openpmd", "genesis"), "w") as f:
+        self.slice.slices = n_slice
+        f["betax"] = self.slice.slice_beta_x.val
+        f["alphax"] = self.slice.slice_alpha_x.val
+        f["betay"] = self.slice.slice_beta_y.val
+        f["alphay"] = self.slice.slice_alpha_y.val
+        f["gamma"] = self.slice.slice_momentum.val / pmd_units.m_e
+        f["delgam"] = self.slice.slice_relative_momentum_spread.val
+        f["ex"] = self.slice.slice_normalized_horizontal_emittance.val
+        f["ey"] = self.slice.slice_normalized_vertical_emittance.val
+        f["t"] = self.slice.slice_bins - np.min(self.slice.slice_bins)
     write_genesis4_beam(pg, filename.replace('openpmd', 'genesis'), n_slice=n_slice)