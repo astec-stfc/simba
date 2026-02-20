0e4355989898a460ff3cb69d18ad9dd3e839dc8a
Alex Brynes <alexander.brynes@stfc.ac.uk>
2026-02-20 15:40:03 +0000

    push b4 home

--- simba/Codes/Polylithic/Polylithic.py
+++ simba/Codes/Polylithic/Polylithic.py
@@ -3,7 +3,7 @@ from ...Modules import Beams as rbf
 import json, requests
 from confluent_kafka import Consumer, Message
 import time, uuid
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, ValidationError
 import numpy as np
 import inspect
 from io import BytesIO
@@ -33,14 +33,31 @@ class polylithicLattice(frameworkLattice):
         """
         Model parameters from the polylithic fastapi
         """
-        lattice_section: str
+        model: str
         """Lattice section in which to execute the model"""
 
-        beam_properties: list[str]
-        """List of beam properties passed to the model"""
+        input_schema: dict
+        """Dict of beam and machine properties passed to the model"""
 
-        machine_settings: list[str]
-        """List of machine settings required by the model"""
+        output_schema: dict
+        """Dict of predicted outputs provided by the model"""
+
+        @field_validator("input_schema", mode="before")
+        @classmethod
+        def validate_input_schema(cls, value: dict) -> dict:
+            """Validate the input schema to ensure it takes the appropriate values."""
+            for prop in ["beam_properties", "machine_settings"]:
+                if prop not in value:
+                    raise ValueError(f"Model {cls.model} must take {prop} as input")
+            return value
+
+        @field_validator("output_schema", mode="before")
+        @classmethod
+        def validate_output_schema(cls, value: dict) -> dict:
+            """Validate the output schema to ensure it outputs some form of beam data."""
+            if "twiss" not in value and "beam" not in value:
+                raise ValueError(f"Model {cls.model} must take return at least either `beam` or `twiss`")
+            return value
 
     def model_post_init(self, __context):
         super().model_post_init(__context)
@@ -53,28 +70,6 @@ class polylithicLattice(frameworkLattice):
                 and "particle_definition" in self.file_block["input"]
         ):
             self.particle_definition = "laser"
-
-    # def endScreen(self, **kwargs):
-    #     """
-    #     Make the final position in the lattice a
-    #     :class:`~SimulationFramework.Elements.screen.screen` object.
-    #
-    #     Returns
-    #     -------
-    #     :class:`~SimulationFramework.Elements.screen.screen`
-    #         The final screen in the lattice
-    #
-    #     """
-    #     return screen(
-    #         name=self.endObject.objectname,
-    #         type="screen",
-    #         centre=self.endObject.centre,
-    #         position_start=self.endObject.position_start,
-    #         position_end=self.endObject.position_start,
-    #         global_rotation=self.endObject.global_rotation,
-    #         global_parameters=self.global_parameters,
-    #         **kwargs,
-    #     )
 
     def writeElements(self):
         """Not implemented for this class"""
@@ -147,52 +142,45 @@ class polylithicLattice(frameworkLattice):
         # query polylithic fastapi for desired beam format using requests
         url = f"{self.executables.polylithic_url}/get_settings/{self.model}"
         try:
-            if debug_no_remote_server:
-                model_requirements = self.ModelParams(
-                    lattice_section=self.objectname,
-                    beam_properties=['data'],
-                    machine_settings=[],
-                )
-            else:
-                response = requests.get(url, timeout=10)
-                response.raise_for_status()
-                json_string = response.text
-                model_requirements = self.ModelParams.model_validate_json(json_string)
-                # if self.objectname != model_requirements.lattice_section:
-                #     raise ValueError("Specified model does not match the lattice section, did you use the wrong model?")
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            json_string = response.text
+            model_requirements = self.ModelParams.model_validate_json(json_string)
+            # if self.objectname != model_requirements.lattice_section:
+            #     raise ValueError("Specified model does not match the lattice section, did you use the wrong model?")
         except requests.RequestException as e:
             raise RuntimeError(f"Failed to query polylithic FastAPI: {e}")
 
         # format beam for requested model data
-        beam: rbf.beam = self.global_parameters["beam"]
         beamdata = {}
-        emitprops = [name for name, value in inspect.getmembers(type(beam.emittance)) if isinstance(value, property)]
-        dataprops = ['x', 'cpx', 'y', 'cpy', 'z', 'cpz']
-        sliceprops = [name for name, value in inspect.getmembers(type(beam.slice)) if isinstance(value, property)]
-        sigmaprops = [name for name, value in inspect.getmembers(type(beam.sigmas)) if isinstance(value, property)]
-        centroidprops = [name for name, value in inspect.getmembers(type(beam.centroids)) if
-                         isinstance(value, property)]
-        twissprops = [name for name, value in inspect.getmembers(type(beam.twiss)) if isinstance(value, property)]
 
         beamcopy = deepcopy(self.global_parameters["beam"])
-        for key in model_requirements.beam_properties:
+        for key in model_requirements.input_schema["beam_properties"]:
             splitkey = key.split(':')
             if len(splitkey) == 2:
                 val = getattr(getattr(beamcopy, splitkey[0]), splitkey[1]).val
                 if val.ndim == 0:
                     val = float(val)
-                beamdata.append(val)
+                beamdata.update({key: val})
             elif len(splitkey) == 3:
                 beamattr = getattr(beamcopy, splitkey[0])
-                arg = [getattr(beamcopy, beamattr) for a in splitkey[2].split(',')]
+                arg = [getattr(beamattr, a) for a in splitkey[2].split(',')]
                 val = getattr(beamattr, splitkey[1])(arg[0], arg[1]).val
                 if val.ndim == 0:
                     val = float(val)
-                beamdata.append(val)
+                beamdata.update({key: val})
 
+        machinedata = {}
+        for key in model_requirements.input_schema["machine_settings"]:
+            splitkey = key.split(':')
+            if len(splitkey) != 2:
+                raise ValueError(f"machine_settings must be indexed by elem_name:parameter, not {key}")
+            if splitkey[0] not in self.elements:
+                raise KeyError(f"Element {splitkey[0]} not found in lattice {self.name}")
+            machinedata.update({key: getattr(self.elementObjects[splitkey[0]], splitkey[1])})
 
         # send kafka the model name, the formatted beam, lattice, and lattice settings
-        url = f"{self.executables.polylithic_url}/submit_lattice/{self.model}"
+        url = f"{self.executables.polylithic_url}/v2/submit_job/{self.model}"
         job_payload = {
             "model": self.model,
             "beam": {},
@@ -200,93 +188,90 @@ class polylithicLattice(frameworkLattice):
             "job_id": self.model + uuid.uuid4().hex,
             "lattice": {"lattice": "example"}
         }
-        if not debug_no_remote_server:
-            try:
-                response = requests.post(url, json=job_payload, timeout=10)
-                response.raise_for_status()
-            except requests.RequestException as e:
-                raise RuntimeError(f"Failed to schedule job on polylithic FastAPI: {e}")
+        try:
+            response = requests.post(url, json=job_payload, timeout=10)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            raise RuntimeError(f"Failed to schedule job on polylithic FastAPI: {e}")
 
-            consumer: Consumer = Consumer({'bootstrap.servers': self.executables.kafka_url, 'group.id': "simframe"})
-            consumer.subscribe(['pl_job_results'])
-            # wait for consumer to return
-            start = time.time()
-            while True:
-                msg: Message = consumer.poll(1.0)
-                print("poll")
-                if time.time() - start > 60:
-                    raise TimeoutError("Timeout while waiting for kafka job")
-                if msg is None:
-                    continue
-                if msg.error():
-                    raise ValueError(f"Polylithic kafka consumer error: {msg.error()}")
+        consumer: Consumer = Consumer({'bootstrap.servers': self.executables.kafka_url, 'group.id': "simframe"})
+        consumer.subscribe(['pl_job_results'])
+        # wait for consumer to return
+        start = time.time()
+        while True:
+            msg: Message = consumer.poll(1.0)
+            print("poll")
+            if time.time() - start > 60:
+                raise TimeoutError("Timeout while waiting for kafka job")
+            if msg is None:
+                continue
+            if msg.error():
+                raise ValueError(f"Polylithic kafka consumer error: {msg.error()}")
 
-                msgvalue = msg.value().decode('utf-8')
-                print('Received message: {}'.format(msgvalue))
-                # convert msgvalue to json
+            msgvalue = msg.value().decode('utf-8')
+            print('Received message: {}'.format(msgvalue))
+            # convert msgvalue to json
 
-                msgvalue = json.loads(msgvalue)
-                if msgvalue["job_id"] == job_payload["job_id"]:
-                    print("job complete")
-                    break
-            consumer.close()
+            msgvalue = json.loads(msgvalue)
+            if msgvalue["job_id"] == job_payload["job_id"]:
+                print("job complete")
+                break
+        consumer.close()
 
-            url = f"{self.executables.polylithic_url}/get_result/{job_payload["job_id"]}"
-            try:
-                response = requests.get(url, timeout=10)
-                response.raise_for_status()
-                return_json = response.json()
-            except requests.RequestException as e:
-                raise RuntimeError(f"Failed to query result from polylithic FastAPI: {e}")
-        else:
-            return_json = job_payload  # for testing purposes, just return the job payload
+        url = f"{self.executables.polylithic_url}/v2/get_result/{job_payload["job_id"]}"
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            return_json = response.json()
+        except requests.RequestException as e:
+            raise RuntimeError(f"Failed to query result from polylithic FastAPI: {e}")
 
         return_beam = deepcopy(self.global_parameters["beam"])
 
-        for key in return_json["beam"]:
-            match key:
-                case "data":
-                    for p in dataprops:
-                        try:
-                            setattr(return_beam.beam, p, np.load(BytesIO(base64.b64decode(return_json["data"][p]))))
-                        except:
-                            pass
-                case "emittance":
-                    for p in emitprops:
-                        try:
-                            setattr(return_beam.emittance, p,
-                                    np.load(BytesIO(base64.b64decode(return_json["emittance"][p]))))
-                        except:
-                            pass
-                case "twiss":
-                    for p in twissprops:
-                        try:
-                            setattr(return_beam.twiss, p, np.load(BytesIO(base64.b64decode(return_json["twiss"][p]))))
-                        except:
-                            pass
-                case "slice":
-                    for p in sliceprops:
-                        try:
-                            setattr(return_beam.slice, p, np.load(BytesIO(base64.b64decode(return_json["slice"][p]))))
-                        except:
-                            pass
-                case "sigmas":
-                    for p in sigmaprops:
-                        try:
-                            setattr(return_beam.sigmas, p, np.load(BytesIO(base64.b64decode(return_json["sigmas"][p]))))
-                        except:
-                            pass
-                case "centroids":
-                    for p in centroidprops:
-                        try:
-                            setattr(return_beam.centroids, p,
-                                    np.load(BytesIO(base64.b64decode(return_json["centroids"][p]))))
-                        except:
-                            pass
-                case "kde":
-                    raise NotImplementedError()
-                case "mve":
-                    raise NotImplementedError()
+        # for key in return_json["beam"]:
+        #     match key:
+        #         case "data":
+        #             for p in dataprops:
+        #                 try:
+        #                     setattr(return_beam.beam, p, np.load(BytesIO(base64.b64decode(return_json["data"][p]))))
+        #                 except:
+        #                     pass
+        #         case "emittance":
+        #             for p in emitprops:
+        #                 try:
+        #                     setattr(return_beam.emittance, p,
+        #                             np.load(BytesIO(base64.b64decode(return_json["emittance"][p]))))
+        #                 except:
+        #                     pass
+        #         case "twiss":
+        #             for p in twissprops:
+        #                 try:
+        #                     setattr(return_beam.twiss, p, np.load(BytesIO(base64.b64decode(return_json["twiss"][p]))))
+        #                 except:
+        #                     pass
+        #         case "slice":
+        #             for p in sliceprops:
+        #                 try:
+        #                     setattr(return_beam.slice, p, np.load(BytesIO(base64.b64decode(return_json["slice"][p]))))
+        #                 except:
+        #                     pass
+        #         case "sigmas":
+        #             for p in sigmaprops:
+        #                 try:
+        #                     setattr(return_beam.sigmas, p, np.load(BytesIO(base64.b64decode(return_json["sigmas"][p]))))
+        #                 except:
+        #                     pass
+        #         case "centroids":
+        #             for p in centroidprops:
+        #                 try:
+        #                     setattr(return_beam.centroids, p,
+        #                             np.load(BytesIO(base64.b64decode(return_json["centroids"][p]))))
+        #                 except:
+        #                     pass
+        #         case "kde":
+        #             raise NotImplementedError()
+        #         case "mve":
+        #             raise NotImplementedError()
 
         self.global_parameters["beam"] = return_beam
 
