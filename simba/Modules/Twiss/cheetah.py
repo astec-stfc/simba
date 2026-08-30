fixed bugs w/ twiss readers: kinetic energy not calculated correctly; ocelot included factor of elementary_charge. also ocelot/cheetah dipole exit fringe not calculated correctly. twiss rematch applied for bmad/ocelot

simba/Modules/Twiss/cheetah.py
@@ -27,7 +27,7 @@ def read_cheetah_twiss_files(self, filename, reset=True):
             self.cp.val = np.append(self.cp.val, cp)
             self.mean_cp.val = np.append(self.mean_cp.val, cp)
             ke = np.array(
-                (np.sqrt(self.E0 ** 2 + cp ** 2) - self.E0 ** 2)
+                (np.sqrt(self.E0_eV ** 2 + cp ** 2) - self.E0_eV)
             )
             self.kinetic_energy.val = np.append(self.kinetic_energy.val, ke)
             gamma = 1 + ke / self.E0_eV

simba/Modules/Twiss/gpt.py
@@ -54,7 +54,7 @@ def read_gdf_twiss_files(self, filename=None, gdfbeam=None, reset=True):
         cp = self.E0 * np.sqrt(gdfbeamdata.avgG**2 - 1)
         # self.cp.val = np.append(self.cp.val, cp / constants.elementary_charge)
         ke = np.array(
-            (np.sqrt(self.E0**2 + cp**2) - self.E0**2) / constants.elementary_charge
+            (np.sqrt(self.E0**2 + cp**2) - self.E0) / constants.elementary_charge
         )
         self.kinetic_energy.val = np.append(self.kinetic_energy.val, ke)
         gamma = 1 + ke / self.E0_eV

simba/Modules/Twiss/ocelot.py
@@ -70,7 +70,7 @@ def interpret_ocelot_data(self, lattice_name, fdat):
     cp = np.sqrt(E**2 - self.E0_eV**2)
     # self.append('cp', cp)
     ke = np.array(
-        (np.sqrt(self.E0**2 + cp**2) - self.E0**2)
+        (np.sqrt(self.E0_eV**2 + cp**2) - self.E0_eV)
     )
     self.kinetic_energy.val = np.append(self.kinetic_energy.val, ke)
     gamma = 1 + ke / self.E0_eV
@@ -117,7 +117,7 @@ def interpret_ocelot_data(self, lattice_name, fdat):
     self.sigma_z.val = np.append(self.sigma_z.val, np.sqrt(fdat["tautau"]) * beta)
     # self.append('sigma_cp', elegantData['Sdelta'] * cp )
     self.sigma_cp.val = np.append(
-        self.sigma_cp.val, np.sqrt(fdat["pp"]) * cp / constants.elementary_charge
+        self.sigma_cp.val, np.sqrt(fdat["pp"]) * cp
     )
     self.mean_cp.val = np.append(self.mean_cp.val, cp)
     # print('elegant = ', (elegantData['Sdelta'] * cp / constants.elementary_charge)[-1)

simba/Modules/Twiss/opal.py
@@ -29,7 +29,7 @@ def read_opal_twiss_files(self, filename, startS=0, reset=True):
             cp = opalData["ref_pz"][()]
             # self.append('cp', cp)
             ke = np.array(
-                (np.sqrt(self.E0**2 + cp**2) - self.E0**2) / constants.elementary_charge
+                (np.sqrt(self.E0**2 + cp**2) - self.E0) / constants.elementary_charge
             )
             self.kinetic_energy.val = np.append(self.kinetic_energy.val, ke)
             gamma = 1 + ke / self.E0_eV